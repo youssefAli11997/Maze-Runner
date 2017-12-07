d8e531df6c1c1bc59ac82b4e7eee9c3a73621282
Muhammad Sharaf <m.sharf47@gmail.com>
2017-12-07 18:34:04 +0200

    adding constants

---
+++ src/constants/Contact.java
@@ -0,0 +1,26 @@
+package constants;
+
+/**
+ * Created by M.Sharaf on 07/12/2017.
+ */
+public class Contact {
+    public class cellType{
+        public static final int DEFAULT = 0;
+        public static final int WALL = 1;
+        public static final int BOMB = 2;
+        public static final int GIFT = 3;
+    }
+
+    public class bombType{
+
+    }
+
+    public class giftType{
+
+    }
+
+    public class weaponType{
+
+    }
+
+}
