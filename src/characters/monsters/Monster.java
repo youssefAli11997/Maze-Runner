3e9173d5c65e0ac0daa12b7a9cea41881e472229
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-12-09 22:05:06 +0200

    Removing left Comments from conflicts !!

--- src/characters/monsters/Monster.java
+++ src/characters/monsters/Monster.java
@@ -3,7 +3,6 @@ package characters.monsters;
 import characters.GameCharacter;
 
 public abstract class Monster extends GameCharacter {
-<<<<<<< HEAD
 	public Monster(int currentRow, int currentColumn) {
 		super(currentRow, currentColumn);
 	}
