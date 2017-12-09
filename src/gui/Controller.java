8a07470f6a2b39ca37d46e31fe14dbaf1681ae43
Muhammad Sharaf <m.sharf47@gmail.com>
2017-12-09 22:31:23 +0200

    adding base gui

---
+++ src/gui/Controller.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by M.Sharaf on 09/12/2017.
+ */
+public class Controller {
+
+
+
+}
