d1810e20d638e8375f1f204b95f42663d4b00243
Youssef_Ali <youssefali6212@gmail.com>
2017-12-09 23:26:03 +0200

    Game Engine Skeleton

--- src/game_engine/GameEngine.java
+++ src/game_engine/GameEngine.java
@@ -1,5 +1,40 @@
 package game_engine;
 
-public class GameEngine {
+import cells.Cell;
+import characters.GameCharacter;
+import characters.players.Player;
+import game_engine.MazeGenerator.MazeGenerator;
 
+
+public class GameEngine {
+    Cell[][] maze;
+    GameCharacter player;
+
+    public GameEngine(int rows, int columns){ // like setup in processing
+        start(rows, columns);
+    }
+
+    public void start(int rows, int columns){
+        maze = new MazeGenerator().create(rows, columns);
+        player = new Player(0,0);
+    }
+
+    public void loop(){ // like draw in processing
+        //process input
+        //update
+        //render
+
+
+
+
+
+        loop();
+    }
+
+    public static void main(String[] args){
+        GameEngine ge = new GameEngine(10,10);
+        while(true){
+            ge.loop();
+        }
+    }
 }
